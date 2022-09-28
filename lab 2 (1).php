1d205cb996549b913a4d1c0c96ddce2bad6f7dfb
nducduyph <109576851+nducduyph@users.noreply.github.com>
2022-09-28 12:33:39 +0700

    Add files via upload

---
+++ lab 2 (1).php
@@ -0,0 +1,45 @@
+<?php
+
+interface PhepTinh {
+    public function phepCong();
+    public function phepTru();
+    public function phepNhan();
+    public function phepChia();
+}
+class TinhToan implements PhepTinh {
+    private $a;
+    private $b;
+
+    public function __construct($a, $b) {
+        $this->a = $a;
+        $this->b = $b;
+    }
+
+    function phepCong() {
+        return $this->a + $this->b;
+    }
+
+    function phepTru() {
+        return $this->a - $this->b;
+    }
+
+    function phepNhan() {
+        return $this->a * $this->b;
+    }
+
+    function phepChia() {
+        if (!$this->b == 0) {
+            return $this->a + $this->b;
+        } else {
+            return 'Không thực hiện được phép tính';
+        }
+    }
+}
+
+$demo = new TinhToan(2, 33);
+
+echo '<pre>';
+var_dump($demo->phepCong());
+var_dump($demo->phepTru());
+var_dump($demo->phepNhan());
+var_dump($demo->phepChia());
